d8a80db92ea9cdcf497ab6531e0d0dd575da9924
Karol Kraus <karolkraus@MacBook-Pro-Karol-2.local>
2024-07-24 14:26:44 +0200

    add: registration code

--- servers/apps/users/src/dto/user.dto.ts
+++ servers/apps/users/src/dto/user.dto.ts
@@ -3,20 +3,27 @@ import {IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator'
 
 @InputType()
 export class RegisterDto {
-    @Field()
-    @IsNotEmpty({message: "Name is required"})
-    @IsString({message: 'Name must need to be one string'})
-    name: string
+  @Field()
+  @IsNotEmpty({ message: 'Name is required' })
+  @IsString({ message: 'Name must need to be one string' })
+  name: string;
 
-    @Field()
-    @IsNotEmpty({message: "Passoword is required"})
-    @MinLength(8, {message: "Passoword must be at least 8 characters."})
-    password: string
+  @Field()
+  @IsNotEmpty({ message: 'Passoword is required' })
+  @MinLength(8, { message: 'Passoword must be at least 8 characters.' })
+  password: string;
 
-    @Field()
-    @IsNotEmpty({message: "Email is required"})
-    @IsEmail({}, {message: "Email is invalid"})
-    email: string;
+  @Field()
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Email is invalid' })
+  email: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Email is required' })
+  phone_number: number;
+
+  @Field()
+  address: string;
 }
 
 @InputType()
--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -4,50 +4,103 @@ import { ConfigService } from '@nestjs/config';
 import { LoginDto, RegisterDto } from './dto/user.dto';
 import { Response } from 'express';
 import { PrismaService } from '../../../prisma/Prisma.service';
+import bcrypt from 'bcrypt';
+
+interface UserData {
+  name: string;
+  email: string;
+  password: string;
+  phone_number: number;
+}
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly prisma: PrismaService,
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
   ) {}
 
   // register user
-  async register(registerDto: RegisterDto, response: Response){
-    const {name, email, password} = registerDto;
+  async register(registerDto: RegisterDto, response: Response) {
+    const { name, email, password, phone_number } = registerDto;
+
     const isEmailExist = await this.prisma.user.findUnique({
       where: {
-        email
+        email,
       },
     });
 
-    if(isEmailExist) {
-      throw new BadRequestException("User already exist with this email")
+    if (isEmailExist) {
+      throw new BadRequestException('User already exist with this email');
     }
-    const user = await this.prisma.user.create({
-      data :{
-        name,
-        email,
-        password
-      }
-    })
-    return {user, response};
+
+    const isPhoneNumberExist = await this.prisma.user.findUnique({
+      where: {
+        phone_number,
+      },
+    });
+
+    if (isPhoneNumberExist) {
+      throw new BadRequestException(
+        'User already exist with this phone number',
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+      phone_number,
+    };
+
+    const activationToken = await this.createActivatationToken(user);
+    const activationCode = activationToken.activationCode;
+
+    console.log(activationCode);
+
+    // const user = await this.prisma.user.create({
+    //   data: {
+    //     name,
+    //     email,
+    //     password: hashedPassword,
+    //     phone_number,
+    //   },
+    // });
+    return { user, response };
+  }
+
+  async createActivatationToken(user: UserData) {
+    const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+    const token = this.jwtService.sign(
+      {
+        user,
+        activationCode,
+      },
+      {
+        secret: this.configService.get<string>('ACTIVATION_SECRET'),
+        expiresIn: '5m',
+      },
+    );
+
+    return { token, activationCode };
   }
 
   // login service
-  async Login(loginDto: LoginDto){
-    const {email, password} = loginDto
+  async Login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
     const user = {
       email,
-      password
-    }
+      password,
+    };
 
     return user;
   }
 
   // get all users
-  async getUsers(){
-    return this.prisma.user.findMany({})
+  async getUsers() {
+    return this.prisma.user.findMany({});
   }
 }
