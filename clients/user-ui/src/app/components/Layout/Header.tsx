81236aa395f38b3ac842bbbc35991f42e64cf2b3
sir-hector <krauskarol7@gmail.com>
2024-09-07 21:17:31 +0200

    Hero component and styles

--- clients/user-ui/src/app/components/Layout/Header.tsx
+++ clients/user-ui/src/app/components/Layout/Header.tsx
@@ -5,7 +5,7 @@ import ProfileDropDown from "../ProfileDropDown";
 
 const Header = () => {
   return (
-    <header className="w-full bg-[#0F1524]">
+    <header className="w-full bg-[#0A0713]">
       <div className="w-[90%] m-auto  h-[80px] flex items-center justify-between">
         <h1 className={`${styles.logo}`}>Food delivery</h1>
         <NavItems activeItem={1} />
--- clients/user-ui/src/app/screens/HomeScreen.tsx
+++ clients/user-ui/src/app/screens/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Layout/Header";
+import Hero from "../views/Root/Hero";
 
 const HomeScreen = () => {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 };
